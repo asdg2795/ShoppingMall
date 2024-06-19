0d26e0557d8aacf026bbe7fb2e039928c15e0168
김준수 <asdg2795@naver.com>
2024-06-19 15:59:49 +0900

    상품 Entity 설계

---
+++ src/main/java/com/example/shoppingmaill/constant/ItemSellStatus.java
@@ -0,0 +1,5 @@
+package com.example.shoppingmaill.constant;
+
+public enum ItemSellStatus {
+    SELL, SOLD_OUT
+}
---
+++ src/main/java/com/example/shoppingmaill/entity/Item.java
@@ -0,0 +1,38 @@
+package com.example.shoppingmaill.entity;
+
+import com.example.shoppingmaill.constant.ItemSellStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "item")
+@Setter
+@ToString
+public class Item extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "item_id")
+    private Long id;                            // 상품 코드
+
+    @Column(nullable = false, length = 50)
+    private String itemNm;                      // 상품명
+
+    @Column(name = "price", nullable = false)
+    private int price;                          // 상품 가격
+
+    @Column(nullable = false)
+    private int stockNumber;                    // 재고 수량
+
+    @Lob
+    @Column(nullable = false)
+    private String itemDetail;                  // 상품 상세 설명
+
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus;      // 상품 판매 상태
+}
