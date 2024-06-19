4bd0f1ff0593a495baf5c56af7bcd53e55f59a1e
김준수 <asdg2795@naver.com>
2024-06-19 15:56:45 +0900

    기본 걸정 값

---
+++ src/main/java/com/example/shoppingmaill/ShoppingMaillApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoppingmaill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMaillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingMaillApplication.class, args);
+    }
+
+}
