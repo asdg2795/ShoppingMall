b54ab37bd256da494d6749ece45225f6ff4f10b2
김준수 <asdg2795@naver.com>
2024-06-19 16:11:08 +0900

    Spring Security6

---
+++ src/main/java/com/example/shoppingmaill/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.example.shoppingmaill.config;
+
+import com.example.shoppingmaill.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  {
+
+    @Autowired
+    MemberService memberService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http
+                .authorizeHttpRequests(authorize -> authorize
+
+                        .requestMatchers("/","/member/","/member/login","/member/logout","/member/new","/member/login/fail","/admin/item/new").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()// 모든 요청에 대해 접근을 허용
+                )
+                .formLogin(login->login
+                        .loginPage("/member/login")
+                        .defaultSuccessUrl("/", true)
+                        .usernameParameter("email")
+                        .failureUrl("/member/login/fail")
+                )
+                .logout(logout->logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+                        .logoutSuccessUrl("/")
+                );
+            return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/css/**", "/js/**", "/img/**");
+    }
+}
