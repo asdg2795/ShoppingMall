79f0fedbaedce887f3db234f4b00b453b23208df
김준수 <asdg2795@naver.com>
2024-07-01 15:47:03 +0900

    상품 조회 목록 SecurityConfig 작성

--- src/main/java/com/example/shoppingmaill/config/SecurityConfig.java
+++ src/main/java/com/example/shoppingmaill/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig  {
         http
                 .authorizeHttpRequests(authorize -> authorize
 
-                        .requestMatchers("/","/member/","/member/login","/member/logout","/member/new","/member/login/fail","/admin/item/new","/admin/item/{itemId}").permitAll()
+                        .requestMatchers("/","/member/","/member/login","/member/logout","/member/new","/member/login/fail","/admin/item/new","/admin/item/{itemId}", "/admin/items", "/admin/items/").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()// 모든 요청에 대해 접근을 허용
                 )
