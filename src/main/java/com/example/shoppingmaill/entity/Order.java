3c24a0ca629b3276a81f42da81f8f86086b36686
김준수 <asdg2795@naver.com>
2024-07-02 16:24:42 +0900

    Order 객체  - OrderItem 객체를 연결하고 OrderItem 객체에 자신을 연결하는 메소드 추가  - OrderItem 객체를 이용하여 주문 객체를 만드는 메소드 추가  - 각 주문 상품의 TotalPrice 를 구한 뒤 모 두 더하는 메소드 추가

--- src/main/java/com/example/shoppingmaill/entity/Order.java
+++ src/main/java/com/example/shoppingmaill/entity/Order.java
@@ -31,5 +31,32 @@ public class Order extends BaseEntity{
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OrderItem> orderItems = new ArrayList<>();
 
+    // Order 객체
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem); // 주문 객체에 주문 상품 객체 연결
+        orderItem.setOrder(this); // 주문 상품 객체에 주문 객체 연결 (연관 관계 주인)
+    }
+
+    public static Order createOrder(Member member, List<OrderItem> orderItemList){
+        Order order = new Order();
+        order.setMember(member);
+        for (OrderItem orderItem : orderItemList) {
+            order.addOrderItem(orderItem);
+        }
+        order.setOrderStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    public int getTotalPrice(){
+        int totalPrice = 0;
+
+        // 각 상품마다 TotalPrice 를 구하고 모두 더함
+        for(OrderItem orderItem : orderItems){
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
+
 
 }
