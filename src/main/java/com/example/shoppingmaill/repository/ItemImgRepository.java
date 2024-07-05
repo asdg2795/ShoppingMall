6f9a248cb961d6414d5b3611aecc24cd2151b822
김준수 <asdg2795@naver.com>
2024-07-05 15:40:50 +0900

    상품 대표 이미지 조회 리포지토리

--- src/main/java/com/example/shoppingmaill/repository/ItemImgRepository.java
+++ src/main/java/com/example/shoppingmaill/repository/ItemImgRepository.java
@@ -1,5 +1,6 @@
 package com.example.shoppingmaill.repository;
 
+import com.example.shoppingmaill.entity.Item;
 import com.example.shoppingmaill.entity.ItemImg;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,6 @@ import java.util.List;
 public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
     // 이미지가 잘 저장되었는지 확인하기 위해 ItemImgRepository 커스텀 조회문 추가
     List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
+
+    ItemImg findByItemIdAndRepImgYn(Long itemId, String repImgYn);
 }
