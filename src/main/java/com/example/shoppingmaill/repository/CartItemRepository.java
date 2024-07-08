4547fef99ebf3e461567d67efededc80d48db3a6
김준수 <asdg2795@naver.com>
2024-07-08 14:55:22 +0900

    - 장바구니 페이지에 전달할 CartDetailDto 리스트를 쿼리 하나로 조회하는 JPQL 문 - 지연 로딩이기 때문에 최적화를 위해서 아래 코드와 같이 DTO 의 생성자를 이용하여 조회 결과를 DTO 객체로 변환 후 반환 할 수 있음

--- src/main/java/com/example/shoppingmaill/repository/CartItemRepository.java
+++ src/main/java/com/example/shoppingmaill/repository/CartItemRepository.java
@@ -1,8 +1,23 @@
 package com.example.shoppingmaill.repository;
 
+import com.example.shoppingmaill.dto.CartDetailDto;
 import com.example.shoppingmaill.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     CartItem findByCartIdAndItemId(Long cartId, Long itemId);
+
+    // select 부분에 new 키워드와 해당 DTO 의 패키지, 클래스명을 지정
+    @Query("select new com.example.shoppingmaill.dto.CartDetailDto(ci.id, i.itemNm, i.price, ci.count, im.imgUrl) " +
+            "from CartItem ci, ItemImg im " +
+            "join ci.item i " +
+            "where ci.cart.id = :cartId " +
+            "and im.item.id = ci.item.id " +
+            "and im.repImgYn = 'Y' " +
+            "order by ci.regTime desc")
+    // 최적화를 위해 한 번에 반환하도록 List<CartDetailDto> 타입 지정
+    List<CartDetailDto> findcartDetailDtoList(Long cartId);
 }
