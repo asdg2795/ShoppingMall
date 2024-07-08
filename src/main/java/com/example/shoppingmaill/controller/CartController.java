38d53d5c83c1ba9e50d76773637be0e34db08685
김준수 <asdg2795@naver.com>
2024-07-08 11:44:20 +0900

    제품 상세 페이지에서 넘어온 CartItemDto 객체와 eamil 을 파라미터로 CartService.addCart() 메소드를 수행

---
+++ src/main/java/com/example/shoppingmaill/controller/CartController.java
@@ -0,0 +1,47 @@
+package com.example.shoppingmaill.controller;
+
+import com.example.shoppingmaill.dto.CartItemDto;
+import com.example.shoppingmaill.service.CartService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class CartController {
+    private CartService cartService;
+
+    @PostMapping(value = "/cart")
+    @ResponseBody
+    public ResponseEntity cart(@RequestBody @Valid CartItemDto cartItemDto,
+                               BindingResult bindingResult, Principal principal) {
+
+        if (bindingResult.hasErrors()) {
+            StringBuilder sb = new StringBuilder();
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+            for (FieldError fieldError : fieldErrors) {
+                sb.append(fieldError.getDefaultMessage());
+            }
+            return new ResponseEntity<String>(sb.toString(), HttpStatus.BAD_REQUEST);
+        }
+
+        Long cartItemId;
+
+        try{
+            cartItemId = cartService.addCart(cartItemDto, principal.getName());
+        } catch (Exception e) {
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<Long>(cartItemId, HttpStatus.OK);
+    }
+}
