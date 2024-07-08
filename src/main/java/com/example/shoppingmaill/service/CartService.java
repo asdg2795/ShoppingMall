a6ff5935bf45569c9a3083b967bd3c58a72c8181
김준수 <asdg2795@naver.com>
2024-07-08 16:22:33 +0900

    장바구니 상품 번호를 파라미터로 받아서 삭제하는 로직 추가

--- src/main/java/com/example/shoppingmaill/service/CartService.java
+++ src/main/java/com/example/shoppingmaill/service/CartService.java
@@ -2,6 +2,8 @@ package com.example.shoppingmaill.service;
 
 import com.example.shoppingmaill.dto.CartDetailDto;
 import com.example.shoppingmaill.dto.CartItemDto;
+import com.example.shoppingmaill.dto.CartOrderDto;
+import com.example.shoppingmaill.dto.OrderDto;
 import com.example.shoppingmaill.entity.Cart;
 import com.example.shoppingmaill.entity.CartItem;
 import com.example.shoppingmaill.entity.Item;
@@ -27,6 +29,7 @@ public class CartService {
     private final CartItemRepository cartItemRepository;
     private final MemberRepository memberRepository;
     private final ItemRepository itemRepository;
+    private final OrderService orderService;
 
     public Long addCart(CartItemDto cartItemDto, String email) {
         Member member = memberRepository.findByEmail(email);
@@ -89,4 +92,31 @@ public class CartService {
         cartItem.updateCount(count);
     }
 
+    public void deleteCartItem(Long cartItemId){
+        CartItem cartItem = cartItemRepository.findById(cartItemId).orElseThrow(EntityNotFoundException::new);
+        cartItemRepository.delete(cartItem);
+    }
+
+    public Long orderCartItem(List<CartOrderDto> cartOrderDtoList, String email){
+        List<OrderDto> orderDtoList = new ArrayList<>();
+
+        // CartOrderDto 객체를 이용하여 cartItem 객체를 조회
+        // cartItem 객체에서 itemId 와 count 값을 이용해 orderDto 객체 생성
+        for (CartOrderDto cartOrderDto : cartOrderDtoList) {
+            CartItem cartItem = cartItemRepository.findById(cartOrderDto.getCartItemId()).orElseThrow(EntityNotFoundException::new);
+            OrderDto orderDto = new OrderDto();
+            orderDto.setItemId(cartItem.getItem().getId());
+            orderDto.setCount(cartItem.getCount());
+            orderDtoList.add(orderDto);
+        }
+
+        Long orderId = orderService.orders(orderDtoList, email);
+
+        // 주문한 장바구니 상품을 제거하는 로직
+        for(CartOrderDto cartOrderDto : cartOrderDtoList) {
+            CartItem cartItem =cartItemRepository.findById(cartOrderDto.getCartItemId()).orElseThrow(EntityNotFoundException::new);
+            cartItemRepository.delete(cartItem);
+        }
+        return orderId;
+    }
 }
